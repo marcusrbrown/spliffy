f7273efc5d94d1a3e36ee82e47f865c23d33bbc7
Marcus R. Brown <contact@marcusrbrown.com>
2021-01-10 14:16:37 -0700

    feat: finish the tutorial

--- index.ts
+++ index.ts
@@ -1,23 +1,33 @@
 import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
 
-const books = new Map<string, any>();
-books.set('1', {
-  id: '1',
-  title: 'The Hound of the Baskervilles',
-  author: 'Conan Doyle, Arthur',
-});
+const getFormattedDate = () => {
+  const date = new Date();
+  return (
+    date.getFullYear() +
+    '-' +
+    (date.getMonth() < 10 ? '0' : '') +
+    (date.getMonth() + 1) +
+    '-' +
+    date.getDate()
+  );
+};
 
 const router = new Router();
 router
   .get('/', (context) => {
     context.response.body = 'Hello world!';
   })
-  .get('/book', (context) => {
-    context.response.body = Array.from(books.values());
-  })
-  .get('/book/:id', (context) => {
-    if (context.params && context.params.id && books.has(context.params.id)) {
-      context.response.body = books.get(context.params.id);
+  .get('/shrt/:id', (context) => {
+    const urls = JSON.parse(Deno.readTextFileSync('./urls.json'));
+    if (context.params && context.params.id && context.params.id in urls) {
+      const link = urls[context.params.id];
+      if (link.expiryDate > getFormattedDate()) {
+        context.response.redirect(link.dest);
+      } else {
+        context.response.body = 'Link expired';
+      }
+    } else {
+      context.response.body = '404';
     }
   });
 
